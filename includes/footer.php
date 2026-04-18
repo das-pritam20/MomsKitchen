d853ee1e62500548cc2012bbb48c064299fdfd3e
Pritam Das <pritam.das.official16@gmail.com>
2026-04-18 23:33:45 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,40 @@
+<!-- #FOOTER -->
+
+    <footer>
+      <div class="footer-wrapper">
+        <a href="#">
+          <img src="./assets/images/mklogo.jpg" alt="logo" class="footer-brand" width="150">
+        </a>
+        <div class="social-link">
+
+          <a href="https://github.com/Samim30" target="_blank">
+            <ion-icon name="logo-github"></ion-icon>
+          </a>
+
+          <a href="https://www.instagram.com/">
+            <ion-icon name="logo-instagram"></ion-icon>
+          </a>
+          <a href="https://www.linkedin.com/in/your-linkedin-profile">
+  <ion-icon name="logo-linkedin"></ion-icon>
+</a>
+
+          </a>
+
+          <a href="https://youtu.be/OTQqj3-Zqi8?si=tT2NfC3Sh7p_UaSS">
+            <ion-icon name="logo-youtube"></ion-icon>
+          </a>
+        </div>
+        <p class="copyright">&copy; Pritam Das. All Rights Reserved.</p>
+      </div>
+    </footer>
+  </div>
+
+  <!--custom js link -->
+  <script src="./assets/js/taaza.js"></script>
+
+  <!-- ionicon link -->
+  <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+  <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+
+</body>
+</html>
